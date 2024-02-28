c6a7fb79ead89f8c74f905c2e0a03b8f594d73fa
Timur <timur0620@list.ru>
2024-02-28 23:51:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Module_15_Console_HomeWork
+{
+    internal class Program
+    {
+        static int count = 0;
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            Task[] tasks = new Task[100];
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = new Task(TaskConsole);
+
+                tasks[i].Start();
+
+                Thread.Sleep(random.Next(10, 100));
+                tasks[i].Wait();
+
+                tasks[i].Dispose();
+            }
+        }
+        static void TaskConsole()
+        {
+            Console.WriteLine($"id -->{Thread.GetCurrentProcessorId()} | count {count++}");
+        }
+    }
+}
